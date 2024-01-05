b843f5c79fd04daf9232220145455f061202be30
Sam Kenyon <sam@metadevo.com>
2024-01-05 02:47:24 -0800

    version 0

---
+++ gui/mainwindow.cpp
@@ -0,0 +1,159 @@
+#include <QContextMenuEvent>
+#include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QFileDialog>
+#include <QFileInfo>
+#include <QTimer>
+
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    changeStylesheet();
+
+
+//    QFont font;
+//    font.setFamily("Courier");
+//    font.setFixedPitch(true);
+//    font.setPointSize(16);
+//    ui->textEdit->setFont(font);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::contextMenuEvent(QContextMenuEvent* event)
+{
+    QMenu menu(this);
+    menu.addAction(ui->actionOpen);
+    menu.addSeparator();
+    menu.addAction(ui->actionSave);
+    menu.addAction(ui->actionSave_As);
+    menu.addSeparator();
+    menu.addAction(ui->actionSettings);
+    menu.exec(event->globalPos());
+}
+
+void MainWindow::changeStylesheet()
+{
+    QString stylesheet;
+    QString fileName = ":/gui/stylesheets/dark_default.qss";
+    qInfo() << "Loading stylesheet " <<  fileName;
+    QFile file(fileName);
+    if(file.open(QIODevice::ReadOnly)) {
+        stylesheet = file.readAll();
+        setStyleSheet(stylesheet);
+    } else {
+        qWarning() << "Error opening stylesheet resource.";
+        return;
+    }
+}
+
+void MainWindow::setStatus(const QString& status)
+{
+    ui->statusLabel->setText(status);
+    QTimer::singleShot(2000, this, &MainWindow::clearStatus);
+}
+
+void MainWindow::clearStatus()
+{
+    ui->statusLabel->clear();
+}
+
+void MainWindow::saveTextFile(const QString& filepath)
+{
+
+    QFileInfo info(filepath);
+    setStatus("Saved file " + info.fileName());
+    ui->filenameLabel->setText(info.fileName());
+}
+
+void MainWindow::openTextFile(const QString& filepath)
+{
+    QString content;
+    QFile file(filepath);
+    if(file.open(QIODevice::ReadOnly)) {
+        content = file.readAll();
+    } else {
+        qWarning() << Q_FUNC_INFO << "Error opening file.";
+        return;
+    }
+
+    QFileInfo info(filepath);
+    QString extension = info.completeSuffix();
+    if (extension == "md" || extension == "markdown") {
+        ui->textEdit->setMarkdown(content);
+    } else {
+        ui->textEdit->setText(content);
+    }
+
+    setStatus("Opened file " + info.fileName());
+    ui->filenameLabel->setText(info.fileName());
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    if (m_currentFilename.isEmpty()) {
+        on_actionSave_As_triggered();
+    } else {
+        saveTextFile(m_currentFilename);
+    }
+}
+
+
+void MainWindow::on_actionSave_As_triggered()
+{
+    QString lastDir;
+    if (m_settings.contains("last_save_dir")) {
+        lastDir = m_settings.value("last_save_dir").toString();
+    } else {
+        lastDir = QDir::home().path();
+    }
+
+    QString filepath = QFileDialog::getSaveFileName(this, tr("Save File"), lastDir, tr("Text Files (*.txt *.md)"));
+    if (!filepath.isEmpty()) {
+        saveTextFile(filepath);
+        m_settings.setValue("last_save_dir", QFileInfo(filepath).canonicalPath());
+    } else {
+        qWarning() << Q_FUNC_INFO << "Error: invalid path";
+    }
+}
+
+
+void MainWindow::on_actionSettings_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString lastDir;
+    if (m_settings.contains("last_open_dir")) {
+        lastDir = m_settings.value("last_open_dir").toString();
+    } else {
+        lastDir = QDir::home().path();
+    }
+
+    QString filepath = QFileDialog::getOpenFileName(this, tr("Open File"), lastDir, tr("Text Files (*.txt *.md *.markdown)"));
+    if (!filepath.isEmpty()) {
+        openTextFile(filepath);
+        m_settings.setValue("last_open_dir", QFileInfo(filepath).canonicalPath());
+    } else {
+        qWarning() << Q_FUNC_INFO << "Error: invalid path";
+    }
+}
+
+
+void MainWindow::on_textEdit_textChanged()
+{
+
+}
+
---
+++ gui/mainwindow.h
@@ -0,0 +1,47 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSettings>
+#include <QThread>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void changeStylesheet();
+    void setStatus(const QString& status);
+    void clearStatus();
+    void saveTextFile(const QString& filepath);
+    void openTextFile(const QString& filepath);
+
+    void on_actionSave_triggered();
+    void on_actionSave_As_triggered();
+    void on_actionSettings_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_textEdit_textChanged();
+
+signals:
+
+
+private:
+    void contextMenuEvent(QContextMenuEvent* event);
+
+    Ui::MainWindow* ui;
+    QSettings m_settings;
+    QThread m_workerThread;
+
+    QString m_currentFilename;
+};
+#endif // MAINWINDOW_H
